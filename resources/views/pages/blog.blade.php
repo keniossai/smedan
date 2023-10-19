4a69787afaaa56ad5a9f687db0d125f7595cc035
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-10-19 02:09:23 +0100

    Create blog.blade.php

---
+++ resources/views/pages/blog.blade.php
@@ -0,0 +1,147 @@
+@extends('layouts.base')
+
+@section('content')
+
+        <!--
+		=============================================
+			Inner Banner
+		==============================================
+		-->
+        <div class="inner-banner-one light-bg text-center pt-200 md-pt-150 pb-85 lg-pb-50 position-relative" style="background-image: url({{ asset('assets/images/media/img_32.jpg') }});">
+            <div class="container position-relative">
+                <div class="row">
+                    <div class="col-xl-6 m-auto">
+                        <ul class="style-none d-inline-flex justify-content-center pager">
+                            <li><a href="index-2.html">Home</a></li>
+                            <li>/</li>
+                            <li><a href="#">Pages</a></li>
+                            <li>/</li>
+                            <li>Blog</li>
+                        </ul>
+                        <h1 class="hero-heading">Explore our News</h1>
+                        <p class="text-lg">Find the team members details here with all the information</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.inner-banner-two -->
+
+
+
+        <!--
+		=====================================================
+			Blog Section Two
+		=====================================================
+		-->
+		<div class="blog-section-two position-relative mt-150 lg-mt-80 mb-150 lg-mb-80">
+			<div class="container">
+				<div class="position-relative">
+					<div class="row gx-xxl-5">
+						<div class="col-md-6">
+							<article class="blog-meta-two mb-80 lg-mb-50 wow fadeInUp">
+								<figure class="post-img rounded-5 position-relative d-flex align-items-end m0" style="background-image: url({{ asset('assets/images/blog/blog_img_03.jpg') }});">
+									<a href="blog-details.html" class="stretched-link rounded-5 date tran3s">09 FEB</a>
+								</figure>
+								<div class="post-data">
+									<div class="d-flex justify-content-between align-items-center flex-wrap">
+										<a href="blog-details.html" class="blog-title"><h4>Spending Habits, 13 Tips for grow Your Money.</h4></a>
+										<a href="blog-details.html" class="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s"><i class="bi bi-arrow-up-right"></i></a>
+									</div>
+									<div class="post-info">Rashed Ka . 6 min . Finance </div>
+								</div>
+							</article>
+							<!-- /.blog-meta-two -->
+						</div>
+						<div class="col-md-6">
+							<article class="blog-meta-two mb-80 lg-mb-50 wow fadeInUp" data-wow-delay="0.1s">
+								<figure class="post-img rounded-5 position-relative d-flex align-items-end m0" style="background-image: url({{ asset('assets/images/blog/blog_img_04.jpg') }});">
+									<a href="blog-details.html" class="stretched-link rounded-5 date tran3s">12 aug</a>
+								</figure>
+								<div class="post-data">
+									<div class="d-flex justify-content-between align-items-center flex-wrap">
+										<a href="blog-details.html" class="blog-title"><h4>Our Travel Card Makes you Happy.</h4></a>
+										<a href="blog-details.html" class="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s"><i class="bi bi-arrow-up-right"></i></a>
+									</div>
+									<div class="post-info">Jubayer Hasan . 7 min read . Travelling</div>
+								</div>
+							</article>
+							<!-- /.blog-meta-two -->
+						</div>
+                        <div class="col-md-6">
+							<article class="blog-meta-two mb-80 lg-mb-50 wow fadeInUp">
+								<figure class="post-img rounded-5 position-relative d-flex align-items-end m0" style="background-image: url({{ asset('assets/images/blog/blog_img_12.jpg') }});">
+									<a href="blog-details.html" class="stretched-link rounded-5 date tran3s">09 FEB</a>
+								</figure>
+								<div class="post-data">
+									<div class="d-flex justify-content-between align-items-center flex-wrap">
+										<a href="blog-details.html" class="blog-title"><h4>It’s easy to Buildup your Business with us</h4></a>
+										<a href="blog-details.html" class="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s"><i class="bi bi-arrow-up-right"></i></a>
+									</div>
+									<div class="post-info">Rashed Ka . 6 min . Finance </div>
+								</div>
+							</article>
+							<!-- /.blog-meta-two -->
+						</div>
+						<div class="col-md-6">
+							<article class="blog-meta-two mb-80 lg-mb-50 wow fadeInUp" data-wow-delay="0.1s">
+								<figure class="post-img rounded-5 position-relative d-flex align-items-end m0" style="background-image: url({{ asset('assets/images/blog/blog_img_13.jpg') }});">
+									<a href="blog-details.html" class="stretched-link rounded-5 date tran3s">12 aug</a>
+								</figure>
+								<div class="post-data">
+									<div class="d-flex justify-content-between align-items-center flex-wrap">
+										<a href="blog-details.html" class="blog-title"><h4>Manage your Online Banking & Get Rewarded</h4></a>
+										<a href="blog-details.html" class="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s"><i class="bi bi-arrow-up-right"></i></a>
+									</div>
+									<div class="post-info">Jubayer Hasan . 7 min read . Travelling</div>
+								</div>
+							</article>
+							<!-- /.blog-meta-two -->
+						</div>
+                        <div class="col-md-6">
+							<article class="blog-meta-two mb-80 lg-mb-50 wow fadeInUp">
+								<figure class="post-img rounded-5 position-relative d-flex align-items-end m0" style="background-image: url({{ asset('assets/images/blog/blog_img_14.jpg') }});">
+									<a href="blog-details.html" class="stretched-link rounded-5 date tran3s">09 FEB</a>
+								</figure>
+								<div class="post-data">
+									<div class="d-flex justify-content-between align-items-center flex-wrap">
+										<a href="blog-details.html" class="blog-title"><h4>It’s easy to Buildup your Business with us</h4></a>
+										<a href="blog-details.html" class="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s"><i class="bi bi-arrow-up-right"></i></a>
+									</div>
+									<div class="post-info">Rashed Ka . 6 min . Finance </div>
+								</div>
+							</article>
+							<!-- /.blog-meta-two -->
+						</div>
+						<div class="col-md-6">
+							<article class="blog-meta-two mb-80 lg-mb-50 wow fadeInUp" data-wow-delay="0.1s">
+								<figure class="post-img rounded-5 position-relative d-flex align-items-end m0" style="background-image: url({{ asset('assets/images/blog/blog_img_15.jpg') }});">
+									<a href="blog-details.html" class="stretched-link rounded-5 date tran3s">12 aug</a>
+								</figure>
+								<div class="post-data">
+									<div class="d-flex justify-content-between align-items-center flex-wrap">
+										<a href="blog-details.html" class="blog-title"><h4>Manage your Online Banking & Get Rewarded</h4></a>
+										<a href="blog-details.html" class="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s"><i class="bi bi-arrow-up-right"></i></a>
+									</div>
+									<div class="post-info">Jubayer Hasan . 7 min read . Travelling</div>
+								</div>
+							</article>
+							<!-- /.blog-meta-two -->
+						</div>
+					</div>
+
+                    <div class="pagination-one mt-20">
+                        <ul class="style-none d-flex align-items-center justify-content-center">
+                            <li><a href="#" class="active">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li>...</li>
+                            <li><a href="#">Last <i class="bi bi-arrow-right"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- /.pagination-one -->
+				</div>
+			</div>
+		</div>
+		<!-- /.blog-section-two -->
+@endsection
