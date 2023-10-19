a964b3a66aaa5ceb8e8f0fb0ce6bd4f4a0461384
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-10-19 02:09:26 +0100

    Create services.blade.php

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="inner-banner-one light-bg text-center pt-200 md-pt-150 pb-85 lg-pb-50 position-relative" style="background-image: url({{ asset('assets/images/media/img_32.jpg') }});">
+    <div class="container position-relative">
+        <div class="row">
+            <div class="col-xl-6 m-auto">
+                <ul class="style-none d-inline-flex justify-content-center pager">
+                    <li><a href="index-2.html">Home</a></li>
+                    <li>/</li>
+                    <li><a href="#">Pages</a></li>
+                    <li>/</li>
+                    <li>Blog</li>
+                </ul>
+                <h1 class="hero-heading">Explore our News</h1>
+                <p class="text-lg">Find the team members details here with all the information</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="block-feature-one position-relative light-bg-deep mt-100 lg-mt-60 pt-120 lg-pt-60 pb-130 lg-pb-60">
+    <div class="container">
+        <div class="position-relative">
+            <div class="title-one text-center mb-40 lg-mb-10">
+                <h2>Our Services</h2>
+                <p class="text-lg mt-20">Inciddnt ut labore et dolor magna aliu. ad mim venam, quis nostru </p>
+            </div>
+            <!-- /.title-one -->
+            <div class="row">
+                <div class="col-lg-4 col-md-6 d-flex wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+                    <div class="card-style-two vstack tran3s w-100 mt-30">
+                        <img src="images/lazy.svg" data-src="images/icon/icon_06.svg" alt="" class="lazy-img icon me-auto">
+                        <h4 class="fw-bold mt-30 mb-25">Expense Report</h4>
+                        <p class="mb-20">Maximize convenience by paying for ads, subscriptions, and inventory with Mastercard®.</p>
+                        <a href="service-details.html" class="arrow-btn tran3s mt-auto stretched-link"><img src="images/lazy.svg" data-src="images/icon/icon_09.svg" alt="" class="lazy-img"></a>
+                    </div>
+                    <!-- /.card-style-two -->
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                    <div class="card-style-two vstack tran3s w-100 mt-30">
+                        <img src="images/lazy.svg" data-src="images/icon/icon_07.svg" alt="" class="lazy-img icon me-auto">
+                        <h4 class="fw-bold mt-30 mb-25">Tax Preparation</h4>
+                        <p class="mb-20">Use receiving accounts in a growing number of currencies and get paid like a local.</p>
+                        <a href="service-details.html" class="arrow-btn tran3s mt-auto stretched-link"><img src="images/lazy.svg" data-src="images/icon/icon_09.svg" alt="" class="lazy-img"></a>
+                    </div>
+                    <!-- /.card-style-two -->
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                    <div class="card-style-two vstack tran3s w-100 mt-30">
+                        <img src="images/lazy.svg" data-src="images/icon/icon_08.svg" alt="" class="lazy-img icon me-auto">
+                        <h4 class="fw-bold mt-30 mb-25">Book keeping</h4>
+                        <p class="mb-20">Use receiving accounts in a growing number of currencies and get paid like a local.</p>
+                        <a href="service-details.html" class="arrow-btn tran3s mt-auto stretched-link"><img src="images/lazy.svg" data-src="images/icon/icon_09.svg" alt="" class="lazy-img"></a>
+                    </div>
+                    <!-- /.card-style-two -->
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+                    <div class="card-style-two vstack tran3s w-100 mt-30">
+                        <img src="images/lazy.svg" data-src="images/icon/icon_59.svg" alt="" class="lazy-img icon me-auto">
+                        <h4 class="fw-bold mt-30 mb-25">Banking</h4>
+                        <p class="mb-20">Maximize convenience by paying for ads, subscriptions, and inventory with Mastercard®.</p>
+                        <a href="service-details.html" class="arrow-btn tran3s mt-auto stretched-link"><img src="images/lazy.svg" data-src="images/icon/icon_09.svg" alt="" class="lazy-img"></a>
+                    </div>
+                    <!-- /.card-style-two -->
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                    <div class="card-style-two vstack tran3s w-100 mt-30">
+                        <img src="images/lazy.svg" data-src="images/icon/icon_60.svg" alt="" class="lazy-img icon me-auto">
+                        <h4 class="fw-bold mt-30 mb-25">Loan Facility</h4>
+                        <p class="mb-20">Use receiving accounts in a growing number of currencies and get paid like a local.</p>
+                        <a href="service-details.html" class="arrow-btn tran3s mt-auto stretched-link"><img src="images/lazy.svg" data-src="images/icon/icon_09.svg" alt="" class="lazy-img"></a>
+                    </div>
+                    <!-- /.card-style-two -->
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                    <div class="card-style-two vstack tran3s w-100 mt-30">
+                        <img src="images/lazy.svg" data-src="images/icon/icon_61.svg" alt="" class="lazy-img icon me-auto">
+                        <h4 class="fw-bold mt-30 mb-25">Consultation</h4>
+                        <p class="mb-20">Use receiving accounts in a growing number of currencies and get paid like a local.</p>
+                        <a href="service-details.html" class="arrow-btn tran3s mt-auto stretched-link"><img src="images/lazy.svg" data-src="images/icon/icon_09.svg" alt="" class="lazy-img"></a>
+                    </div>
+                    <!-- /.card-style-two -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <img src="images/lazy.svg" data-src="images/shape/shape_05.svg" alt="" class="lazy-img shapes shape_01">
+    <img src="images/lazy.svg" data-src="images/shape/shape_05.svg" alt="" class="lazy-img shapes shape_02">
+</div>
+@endsection
