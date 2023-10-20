b2c1546eb53d4afcfb52b5c7ee71e3d00f1ba4b6
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-10-20 15:31:02 +0100

    Update dashboard to see auth users

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,254 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+
+@section('content')
+<div class="main-content">
+    <div class="row">
+      <div class="col-xl-9 col-lg-7">
+        <div class="main-content-title-profile mb-50">
+          <div class="main-content-title">
+            <h3>Hi, {{ Auth::user()->name }}!</h3>
+            <img src="assets/img/dashboard/icon/love-icon.svg" alt />
+          </div>
+          <div class="profile">
+            <a href="">
+              View Profile
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 8 8"
+              >
+                <path
+                  d="M6.669 2.27202L0.94102 8L0 7.05898L5.72731 1.331H0.679478V0H8V7.32052H6.669V2.27202Z"
+                />
+              </svg>
+            </a>
+          </div>
         </div>
+        <div class="counter-area">
+          <div class="row g-3">
+            <div class="col-xl-3 col-md-6">
+              <div class="counter-single">
+                <div class="counter-icon">
+                  <img
+                    src="assets/img/dashboard/icon/counter-active-property-icon.svg"
+                    alt
+                  />
+                </div>
+                <div class="counter-content">
+                  <p>Active Property</p>
+                  <div class="number">
+                    <h3 class="counter">20</h3>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+              <div class="counter-single two">
+                <div class="counter-icon">
+                  <img
+                    src="assets/img/dashboard/icon/counter-expire-property-icon.svg"
+                    alt
+                  />
+                </div>
+                <div class="counter-content">
+                  <p>Expire Property</p>
+                  <div class="number">
+                    <h3 class="counter">30</h3>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+              <div class="counter-single three">
+                <div class="counter-icon">
+                  <img
+                    src="assets/img/dashboard/icon/counter-pending-property-icon.svg"
+                    alt
+                  />
+                </div>
+                <div class="counter-content">
+                  <p>Pending Property</p>
+                  <div class="number">
+                    <h3 class="counter">05</h3>
+                    <span>+</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+              <div class="counter-single four">
+                <div class="counter-icon">
+                  <img
+                    src="assets/img/dashboard/icon/counter-delete-property-icon.svg"
+                    alt
+                  />
+                </div>
+                <div class="counter-content">
+                  <p>Delete Property</p>
+                  <div class="number">
+                    <h3 class="counter">10</h3>
+                    <span>+</span>
+                  </div>
+                </div>
+                <img
+                  src="assets/img/dashboard/icon/counter-area-top-vector.svg"
+                  alt
+                  class="counter-area-vector"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="recent-listing-area">
+          <h6>My Recent Post</h6>
+          <div class="recent-listing-table">
+            <table class="eg-table2">
+              <thead>
+                <tr>
+                  <th>Property Info</th>
+                  <th>When Added</th>
+                  <th>Favourite</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Property Info">
+                    <div class="property-info-wrapper">
+                      <div class="property-info-img">
+                        <img
+                          src="assets/img/dashboard/property-info-img1.png"
+                          alt
+                        />
+                      </div>
+                      <div class="property-info-content">
+                        <div class="location">
+                          <a href="property-listing-left-sidebar.html"
+                            ><i class="bi bi-geo-alt"></i> Panama City</a
+                          >
+                        </div>
+                        <h6>
+                          <a href="property-listing-left-sidebar.html"
+                            >Willow Creek Residence</a
+                          >
+                        </h6>
+                        <a
+                          href="poperty-deatils1.html"
+                          class="view-details"
+                        >
+                          <img
+                            src="assets/img/dashboard/icon/view-details-icon.svg"
+                            alt
+                          />
+                          View Details
+                        </a>
+                      </div>
+                    </div>
+                  </td>
+                  <td data-label="When Added" class="table-product">
+                    <span>02 April, 2023</span>
+                  </td>
+                  <td data-label="Favourite">
+                    <span>100 person</span>
+                  </td>
+                  <td data-label="Action">
+                    <ul class="action-buttons">
+                      <li>
+                        <img
+                          src="{{ asset('assets/images/icon/action-edit-btn.svg') }}"
+                          alt
+                        />
+                      </li>
+                      <li>
+                        <img
+                          src="{{ asset('assets/images/icon/action-delete-btn.svg') }}"
+                          alt
+                        />
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div class="pagination-area">
+              <ul class="paginations">
+                <li class="page-item active">
+                  <a href="#">1</a>
+                </li>
+                <li class="page-item">
+                  <a href="#">2</a>
+                </li>
+                <li class="page-item">
+                  <a href="#">3</a>
+                </li>
+              </ul>
+              <ul class="paginations-buttons">
+                <li>
+                  <a href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="7"
+                      height="14"
+                      viewBox="0 0 7 14"
+                    >
+                      <path
+                        d="M0 7.00008L7 0L2.54545 7.00008L7 14L0 7.00008Z"
+                      />
+                    </svg>
+                    Prev
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Next
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="7"
+                      height="14"
+                      viewBox="0 0 7 14"
+                      fill="none"
+                    >
+                      <path
+                        d="M7 7.00008L0 0L4.45455 7.00008L0 14L7 7.00008Z"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-lg-5">
+        <div class="message-area">
+          <div class="message-area-title">
+            <h6>List of users</h6>
+          </div>
+          <ul class="message-list">
+            <li>
+              <div class="author-area">
+                <div class="author-img">
+                  <img
+                    src="assets/img/dashboard/message-author-img1.png"
+                    alt
+                  />
+                </div>
+                <div class="author-content">
+                  <div class="author-title">
+                    <h6>Jacoline Franqly,</h6>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+
+        </div>
+      </div>
     </div>
-</x-app-layout>
+  </div>
+
+@endsection
