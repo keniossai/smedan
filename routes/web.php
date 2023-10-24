6f9e126d969bc70866352a7c0399df5b5997cfa9
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-10-24 12:17:18 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
@@ -22,16 +23,14 @@ Route::get('/blog', [BlogController::class, 'index']);
 Route::get('/services', [HomeController::class, 'index']);
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/create-post', [PostController::class, 'index']);
+    Route::get('/create-post', [PostController::class, 'create']);
     Route::post('/create-post', [PostController::class, 'store'])->name('post.store');
 });
 
